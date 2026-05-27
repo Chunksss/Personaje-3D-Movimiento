47a6200a4ef2abaf08303aaa4aa98e24a958d9b7
Chunksss <juan.campillo.c@gmail.com>
2026-05-27 10:37:26 -0600

    Menu JP

---
+++ Assets/2D Casual UI/Menu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour 
+{
+    public void IrEscena()
+    {
+        SceneManager.LoadScene("Primera");
+    }
+}
